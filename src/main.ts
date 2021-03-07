495db42c72fb5ec9dfaebb86d18b9fe7ebc55152
Gaston Morixe <gaston@gastonmorixe.com>
2021-03-07 18:23:20 -0300

    Port and Procfile

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  if (process.env.NODE_ENV === 'production') {
+    app.enableCors() // protection
+  }
+  await app.listen(process.env.PORT || 3000)
 }
 bootstrap()
