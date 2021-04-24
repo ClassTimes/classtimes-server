b90e8c7bd53b8ffd307c8d94ff2d6e26983860c0
frankmangone <francomangone18@gmail.com>
2021-04-24 18:56:36 -0300

    MONTHLY rrule WIP

--- src/utils/RRuleParsing.spec.ts
+++ src/utils/RRuleParsing.spec.ts
@@ -1,79 +1,4 @@
-import {
-  arrayCompletePasses,
-  loopIndexForward,
-  parseEndDate,
-  MAX_DATE,
-} from './RRuleParsing'
-
-describe('loopIndexForward', () => {
-  const testArray = [1, 2, 3, 4, 5, 6]
-
-  it(`Array [${testArray}] should return 2 if startIndex=0 and count=2`, () => {
-    expect(loopIndexForward(testArray, 0, 2)).toBe(2)
-  })
-
-  it(`Array [${testArray}] should return 3 if startIndex=4 and count=5`, () => {
-    expect(loopIndexForward(testArray, 4, 5)).toBe(3)
-  })
-
-  it(`Array [${testArray}] should return -1 if startIndex is not an integer`, () => {
-    expect(loopIndexForward(testArray, 0.5, 5)).toBe(-1)
-  })
-
-  it(`Array [${testArray}] should return -1 if startIndex is negative`, () => {
-    expect(loopIndexForward(testArray, -2, 5)).toBe(-1)
-  })
-
-  it(`Array [${testArray}] should return -1 if startIndex is greater than length - 1`, () => {
-    expect(loopIndexForward(testArray, -2, 5)).toBe(-1)
-  })
-
-  it(`Array [${testArray}] should return -1 if count not an integer`, () => {
-    expect(loopIndexForward(testArray, 0.5, 5)).toBe(-1)
-  })
-
-  it(`Array [${testArray}] should return -1 if count is negative`, () => {
-    expect(loopIndexForward(testArray, -2, 5)).toBe(-1)
-  })
-})
-
-// ------------------------------
-
-describe('arrayCompletePasses', () => {
-  const testArray = [1, 2, 3, 4]
-
-  it(`Array [${testArray}] should return 1 with count=10 and startIndex=0`, () => {
-    expect(arrayCompletePasses(testArray, 0, 10)).toBe(1)
-  })
-
-  it(`Array [${testArray}] should return 0 with count=3 and startIndex=3`, () => {
-    expect(arrayCompletePasses(testArray, 3, 3)).toBe(0)
-  })
-
-  it(`Array [${testArray}] should return 0 with count=2 and startIndex=0`, () => {
-    expect(arrayCompletePasses(testArray, 0, 2)).toBe(0)
-  })
-
-  it(`Array [${testArray}] should return -1 if startIndex is not an integer`, () => {
-    expect(arrayCompletePasses(testArray, 0.5, 5)).toBe(-1)
-  })
-
-  it(`Array [${testArray}] should return -1 if startIndex is negative`, () => {
-    expect(arrayCompletePasses(testArray, -2, 5)).toBe(-1)
-  })
-
-  it(`Array [${testArray}] should return -1 if startIndex is greater than length - 1`, () => {
-    expect(arrayCompletePasses(testArray, -2, 5)).toBe(-1)
-  })
-
-  it(`Array [${testArray}] should return -1 if count not an integer`, () => {
-    expect(arrayCompletePasses(testArray, 0.5, 5)).toBe(-1)
-  })
-
-  it(`Array [${testArray}] should return -1 if count is negative`, () => {
-    expect(arrayCompletePasses(testArray, -2, 5)).toBe(-1)
-  })
-})
+import { parseEndDate, MAX_DATE } from './RRuleParsing'
 
 // ------------------------------
 
@@ -166,6 +91,36 @@ describe('parseEndDate', () => {
         const rrule = 'RRULE:FREQ=WEEKLY;COUNT=12;WKST=SU;BYDAY=TU,WE,TH'
         const startDate = new Date('2021-04-20T00:00:00.000Z') // 04/20/2021
         const endDate = new Date('2021-05-13T00:00:00.000Z') // 05/13/2021
+        expect(parseEndDate(startDate, rrule).toISOString()).toBe(
+          endDate.toISOString(),
+        )
+      })
+
+      it('should return Thursday 5 weeks from current week if COUNT=9 and INTERVAL=2', () => {
+        const rrule =
+          'RRULE:FREQ=WEEKLY;COUNT=9;INTERVAL=2;WKST=SU;BYDAY=TU,WE,TH'
+        const startDate = new Date('2021-04-20T00:00:00.000Z') // 04/20/2021
+        const endDate = new Date('2021-05-20T00:00:00.000Z') // 05/20/2021
+        expect(parseEndDate(startDate, rrule).toISOString()).toBe(
+          endDate.toISOString(),
+        )
+      })
+
+      it('should return null if currentDay is not in the specified BYDAY', () => {
+        const rrule = 'RRULE:FREQ=WEEKLY;COUNT=12;WKST=SU;BYDAY=TU,WE,TH'
+        const startDate = new Date('2021-04-19T00:00:00.000Z') // 04/20/2021
+        expect(parseEndDate(startDate, rrule)).toBe(null)
+      })
+    })
+
+    //
+    //
+
+    describe('FREQ=MONTHLY', () => {
+      it('should return first friday in 2 months if count=3 and byday=1FR', () => {
+        const rrule = 'RRULE:FREQ=MONTHLY;COUNT=3;BYDAY=1FR'
+        const startDate = new Date('2021-05-07T00:00:00.000Z') // 05/07/2021
+        const endDate = new Date('2021-07-02T00:00:00.000Z') // 07/02/2021
         expect(parseEndDate(startDate, rrule).toISOString()).toBe(
           endDate.toISOString(),
         )
--- src/utils/RRuleParsing.ts
+++ src/utils/RRuleParsing.ts
@@ -1,5 +1,10 @@
 import { RRule } from 'rrule'
 import dayjs from 'dayjs'
+import {
+  loopIndexForward,
+  loopRestarts,
+  arrayCompletePasses,
+} from './ArrayLooping'
 
 export const MAX_DATE = new Date('3000-01-01T00:00:00.000Z')
 
@@ -19,6 +24,8 @@ export const DAYS = {
   SU: 6,
 }
 
+export const MONTHS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
 export function parseEndDate(startDateUtc, rruleString): Date {
   const rruleObject = RRule.fromString(rruleString)
   const rrule = rruleObject.options
@@ -35,12 +42,14 @@ export function parseEndDate(startDateUtc, rruleString): Date {
      * RRule COUNT
      *
      */
+    const count = rrule.count
     const startDate = dayjs(new Date(startDateUtc))
 
     if (rrule.freq === FREQ.DAILY) {
       const interval = rrule.interval || 1
       return startDate.add((rrule.count - 1) * interval, 'day').toDate()
     }
+    //
     //
     //
     else if (rrule.freq === FREQ.WEEKLY) {
@@ -57,11 +66,11 @@ export function parseEndDate(startDateUtc, rruleString): Date {
           /*
            * Provided startDate *matches* one of the provided weekdays
            */
-          if (rrule.count <= weekdays.length - startDayIndex) {
+          if (count <= weekdays.length - startDayIndex) {
             /*
              *  End Date is in the same week as Start Date
              */
-            const endDayIndex = startDayIndex + rrule.count - 1
+            const endDayIndex = startDayIndex + count - 1
             const additionalDays =
               weekdays[endDayIndex] - weekdays[startDayIndex]
             //
@@ -70,7 +79,6 @@ export function parseEndDate(startDateUtc, rruleString): Date {
             /*
              *  End Date is not in the same week as Start Date
              */
-            const count = rrule.count
             const endDayIndex = loopIndexForward(
               weekdays,
               startDayIndex,
@@ -78,18 +86,29 @@ export function parseEndDate(startDateUtc, rruleString): Date {
             )
             const additionalDays: number =
               7 - weekdays[startDayIndex] + weekdays[endDayIndex]
-            const additionalWeeks: number =
-              (count -
-                (weekdays.length - startDayIndex - 1) -
-                (endDayIndex - 1)) /
-                weekdays.length -
-              1
+            const additionalWeeks: number = arrayCompletePasses(
+              weekdays,
+              startDayIndex,
+              count - 1,
+            )
+            const interval: number = rrule.interval || 1
+            const spanWeeks: number = loopRestarts(
+              weekdays,
+              startDayIndex,
+              count - 1,
+            )
             //
             return startDate
-              .add(additionalWeeks, 'week')
+              .add(additionalWeeks + spanWeeks * (interval - 1), 'week')
               .add(additionalDays, 'day')
               .toDate()
           }
+        } else {
+          /*
+           * Provided startDate *does not match* one of the provided weekdays
+           * Should return an error on save, so return null
+           */
+          return null
         }
       } else {
         /*
@@ -101,11 +120,24 @@ export function parseEndDate(startDateUtc, rruleString): Date {
     }
     //
     //
+    //
     else if (rrule.freq === FREQ.MONTHLY) {
+      const nweekday = rrule.bynweekday
+      if (nweekday) {
+        // TODO: Check coincidence with provided nweekday?
+        const additionalMonths = loopRestarts(nweekday, 0, count - 1)
+      }
+
+      //
+      return null
     }
+    //
+    //
     //
     else if (rrule.freq === FREQ.YEARLY) {
     }
+    //
+    //
     return MAX_DATE
   } else {
     /*
@@ -116,94 +148,59 @@ export function parseEndDate(startDateUtc, rruleString): Date {
 }
 
 //
-//
 
-export const loopIndexForward = (
-  arr: Array<number>,
-  startIndex: number,
-  count: number,
-): number => {
-  /* startIndex is expected to be:
-   *  - An integer
-   *  - Greater or equal than 0
-   *  - Lower than array length
-   */
-  if (
-    !Number.isInteger(startIndex) ||
-    startIndex < 0 ||
-    startIndex >= arr.length
-  ) {
-    return -1
-  }
+function nthDayInNMonths(startDate, nweekdays, count, interval) {
+  const currentMonth = startDate.month()
+  const currentYear = startDate.year()
+  const startMonthIndex = MONTHS.indexOf(currentMonth)
 
-  /* count is expected to be:
-   *  - An integer
-   *  - Greater or equal than 0
-   */
+  const nWeekdayEndIndex = loopIndexForward(nweekdays, 1, count - 1)
+  const nWeekdayEnd = nweekdays[nWeekdayEndIndex]
+  const nMonths = loopRestarts(nweekdays, 1, count - 1) * interval
 
-  if (!Number.isInteger(count) || count < 0) {
-    return -1
-  }
+  const endMonthIndex = loopIndexForward(MONTHS, startMonthIndex, nMonths)
+  const additionalYears = loopRestarts(MONTHS, startMonthIndex, nMonths)
 
-  const endIndex: number = arr.length - 1
-  if (count <= endIndex - startIndex) {
-    return startIndex + count
-  } else {
-    /* adjust count to array start */
-    const countFromStart = count - (endIndex - startIndex + 1)
-    return countFromStart % arr.length
-  }
-}
+  const endMonth = MONTHS[endMonthIndex]
+  const endYear = currentYear + additionalYears
 
-// An alternative (tested):
+  const endMonthAndYear = dayjs().year(endYear).month(endMonth)
 
-// export const loopIndexForward = (
-//   arr: Array<any>,
-//   startIndex: number,
-//   count: number,
-// ): number => {
-//   /* Count is expected to be greater than 0 */
-//   const endIndex: number = arr.length - 1
-//   let loopingIndex: number = startIndex
-//   while (count > 0) {
-//     if (loopingIndex === endIndex) {
-//       loopingIndex = 0
-//     } else {
-//       loopingIndex++
-//     }
-//     count--
-//   }
-//   return loopingIndex
-// }
-
-export const arrayCompletePasses = (
-  arr: Array<any>,
-  startIndex: number,
-  count: number,
-): number => {
   /*
-   * This method counts how many times an array is traversed from start to finish
-   * when counting from a [startIndex], a [count] number of times, with a "looping" count
+   * nweekdays is an array of arrays; the inner arrays are of format [a,b]
+   * where a is the week day (monday=0, tuesday=1 ...), and b represents if it's the first, second, etc day of the month
    *
-   *
-   * For example:
-   * [1, 2, 3] [1, 2, 3] [1, 2, 3] [1, 2, 3] [1, 2, 3]
-   *     ^  ^   ^  ^  ^   ^  ^  ^   ^  ^  ^   ^  ^
-   *     0  1   2  3  4   5  6  7   8  9  10  11 12
-   *
-   * In this case, count = 12 and startIndex = 1
-   * Then the count finished on endIndex = 1, having fully looped through the array 3 times
-   *
-   * In other words, it counts the number of arrays "in the middle"
+   * i.e. [1,1] means first tuesday of the month
    */
-  const endIndex = loopIndexForward(arr, startIndex, count)
-  if (endIndex === -1) {
-    // Invalid arguments
-    return -1
+
+  if (nWeekdayEnd[1] > 0) {
+    const firstDayOfMonth = endMonthAndYear.day(1)
+  } else if (nWeekdayEnd[1] < 0) {
+    // January, March, May, August, October, and December have 31 days
+    let lastDayOfMonth
+    if (
+      endMonth === 0 ||
+      endMonth === 2 ||
+      endMonth === 4 ||
+      endMonth === 7 ||
+      endMonth === 9 ||
+      endMonth === 11
+    ) {
+      lastDayOfMonth = endMonthAndYear.day(31)
+    }
+    // April, June, July, September, and November have 30 days
+    else if (
+      endMonth === 3 ||
+      endMonth === 5 ||
+      endMonth === 6 ||
+      endMonth === 8 ||
+      endMonth === 10
+    ) {
+      lastDayOfMonth = endMonthAndYear.day(30)
+    }
+    // February may have 29 days during leap years
+    else {
+    }
   }
-  if (count === endIndex - startIndex) {
-    // Avoid OBOB-type error
-    return 0
-  }
-  return (count - (arr.length - startIndex - 1) - (endIndex + 1)) / arr.length
+  return null
 }
