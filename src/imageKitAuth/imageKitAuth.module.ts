5a329a562355a33a3044dd1352865a77fcce6b42
frankmangone <francomangone18@gmail.com>
2021-04-30 10:12:49 -0300

    Removed Controller

--- src/imageKitAuth/imageKitAuth.module.ts
+++ src/imageKitAuth/imageKitAuth.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common'
 
-import { ImageKitAuthController } from './imageKitAuth.controller'
 import { ImageKitAuthResolver } from './imageKitAuth.resolver'
 import { ImageKitAuthService } from './imageKitAuth.service'
 
 @Module({
-  controllers: [ImageKitAuthController],
   providers: [ImageKitAuthResolver, ImageKitAuthService],
 })
 export class ImageKitAuthModule {}
