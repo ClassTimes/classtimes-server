85371b8f94c2f09a3ef8f36f5d261483f3768c19
Franco Mangone <francomangone18@gmail.com>
2022-07-09 18:21:11 -0300

    Successfully adds back the GraphQLModule in its entirety

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common'
-// import { join } from 'path'
-// import gitCommitInfo from 'git-commit-info'
-// import * as GQL from '@nestjs/graphql'
+import { join } from 'path'
+import gitCommitInfo from 'git-commit-info'
+import { GraphQLModule } from '@nestjs/graphql'
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { SendGridModule } from '@anchan828/nest-sendgrid'
 import { MongooseModule } from '@nestjs/mongoose'
-// import * as Utils from './utils'
+import * as Utils from './utils'
 import { APP_GUARD } from '@nestjs/core'
 import { GqlAuthGuard } from './auth/gql-auth.guard'
 
-// // Modules
+// Modules
 import { AuthModule } from './auth/auth.module'
 import { CalendarEventModule } from './entities/calendarEvent/calendarEvent.module'
 import { CareerModule } from './entities/career/career.module'
@@ -31,33 +32,34 @@ import { UserModule } from './entities/user/user.module'
     MongooseModule.forRoot(
       process.env.MONGODB || 'mongodb://localhost:27017/classtimes',
     ),
-    // GQL.GraphQLModule.forRoot({
-    //   installSubscriptionHandlers: true,
-    //   autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-    //   sortSchema: true,
-    //   // playground: true,
-    //   debug: false,
-    //   introspection: true, // TODO Remove in production at release time
-    //   context: (options) => {
-    //     const { req } = options
-    //     return { req }
-    //   },
-    //   formatResponse: (response) => {
-    //     let extensions
-    //     if (Utils.isDev) {
-    //       const commit = gitCommitInfo() // information of process.cwd() and the latest commit
-    //       const date = new Date().toISOString()
-    //       extensions = {
-    //         ...extensions,
-    //         env: { date, commit },
-    //       }
-    //     }
-    //     return {
-    //       ...response,
-    //       extensions,
-    //     }
-    //   },
-    // }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      debug: false,
+      playground: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      introspection: true, // TODO Remove in production at release time
+      installSubscriptionHandlers: true,
+      context: (options) => {
+        const { req } = options
+        return { req }
+      },
+      formatResponse: (response) => {
+        let extensions
+        if (Utils.isDev) {
+          const commit = gitCommitInfo() // information of process.cwd() and the latest commit
+          const date = new Date().toISOString()
+          extensions = {
+            ...extensions,
+            env: { date, commit },
+          }
+        }
+        return {
+          ...response,
+          extensions,
+        }
+      },
+    }),
     AuthModule,
     CalendarEventModule,
     CareerModule,
