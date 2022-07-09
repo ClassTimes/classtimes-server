921043352d0a6efab2dc7af2ad01831c7006218c
Franco Mangone <francomangone18@gmail.com>
2022-07-09 18:08:40 -0300

    Activates all modules except for the GraphQL one

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,23 +5,23 @@ import { Module } from '@nestjs/common'
 import { SendGridModule } from '@anchan828/nest-sendgrid'
 import { MongooseModule } from '@nestjs/mongoose'
 // import * as Utils from './utils'
-// import { APP_GUARD } from '@nestjs/core'
-// import { GqlAuthGuard } from './auth/gql-auth.guard'
+import { APP_GUARD } from '@nestjs/core'
+import { GqlAuthGuard } from './auth/gql-auth.guard'
 
 // // Modules
-// import { AuthModule } from './auth/auth.module'
-// import { CalendarEventModule } from './entities/calendarEvent/calendarEvent.module'
-// import { CareerModule } from './entities/career/career.module'
-// import { CaslModule } from './casl/casl.module'
-// import { DiscussionModule } from './entities/discussion/discussion.module'
-// import { EventModule } from './entities/event/event.module'
-// import { FollowModule } from './entities/follow/follow.module'
-// import { InstituteModule } from './entities/institute/institute.module'
+import { AuthModule } from './auth/auth.module'
+import { CalendarEventModule } from './entities/calendarEvent/calendarEvent.module'
+import { CareerModule } from './entities/career/career.module'
+import { CaslModule } from './casl/casl.module'
+import { DiscussionModule } from './entities/discussion/discussion.module'
+import { EventModule } from './entities/event/event.module'
+import { FollowModule } from './entities/follow/follow.module'
+import { InstituteModule } from './entities/institute/institute.module'
 // import { ImageKitAuthModule } from './imageKitAuth/imageKitAuth.module'
-// import { PermissonModule } from './entities/permisson/permisson.module'
-// import { SchoolModule } from './entities/school/school.module'
-// import { SubjectModule } from './entities/subject/subject.module'
-// import { UserModule } from './entities/user/user.module'
+import { PermissonModule } from './entities/permisson/permisson.module'
+import { SchoolModule } from './entities/school/school.module'
+import { SubjectModule } from './entities/subject/subject.module'
+import { UserModule } from './entities/user/user.module'
 
 @Module({
   imports: [
@@ -58,26 +58,26 @@ import { MongooseModule } from '@nestjs/mongoose'
     //     }
     //   },
     // }),
-    // AuthModule,
-    // CalendarEventModule,
-    // CaslModule,
-    // CareerModule,
-    // DiscussionModule,
-    // EventModule,
-    // FollowModule,
-    // InstituteModule,
+    AuthModule,
+    CalendarEventModule,
+    CareerModule,
+    CaslModule,
+    DiscussionModule,
+    EventModule,
+    FollowModule,
+    InstituteModule,
     // ImageKitAuthModule,
-    // PermissonModule,
-    // SchoolModule,
-    // SubjectModule,
-    // UserModule,
+    PermissonModule,
+    SchoolModule,
+    SubjectModule,
+    UserModule,
   ],
   controllers: [],
   providers: [
-    // {
-    //   provide: APP_GUARD,
-    //   useClass: GqlAuthGuard,
-    // },
+    {
+      provide: APP_GUARD,
+      useClass: GqlAuthGuard,
+    },
   ],
 })
 export class AppModule {}
