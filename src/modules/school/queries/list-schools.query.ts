fab454c130aaf96427f3ce97969ce9c1579c6377
Franco Mangone <francomangone18@gmail.com>
2022-07-10 23:22:33 -0300

    Updates listSchools to also include `before`

--- src/modules/school/queries/list-schools.query.ts
+++ src/modules/school/queries/list-schools.query.ts
@@ -2,10 +2,12 @@ const query = `#graphql
   query ListSchools(
     $first: Int,
     $after: String,
+    $before: String,
   ) {
     listSchools(
       first: $first,
       after: $after,
+      before: $before,
     ) {
       __typename
       totalCount
